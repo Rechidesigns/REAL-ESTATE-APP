29246e39f26547c125290f1eac411f7c8c6fac2b
Rechidesigns <alerechiordu@gmail.com>
2022-11-23 23:23:37 +0100

    models

--- realestateapp/models.py
+++ realestateapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Listing(models.Model):
+    title = models.CharField(max_length=150)
+    price = models.IntegerField()
+    numbers_of_bedrooms = models.IntegerField()
+    numbers_of_bathrooms = models.IntegerField()
+    square_footage = models.IntegerField()
+    address = models.CharField(max_length=150)
+    # image = models.ImageField()
+
+    def __str__(self):
+        return self.title
+
