973219a95eed147a9017451564f4cc3868bee08a
Rechidesigns <alerechiordu@gmail.com>
2022-11-28 01:28:52 +0100

    html setup

--- realestate/urls.py
+++ realestate/urls.py
@@ -13,28 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-# from django.urls import path
-# from realestateapp.views import (
-#     listing_list, 
-#     listing_retrieve, 
-#     listing_create,
-#     listing_update,
-#     listing_delete,
-# )
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('', listing_list),
-#     path('listings/<pk>/', listing_retrieve),
-#     path('listings/<pk>/edit/', listing_update),
-#     path('listings/<pk>/delet/', listing_delete),
-#     path('add-listing/', listing_create),
-    
-# ]
-
-
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path
 
 from realestateapp.views import (
@@ -53,3 +34,6 @@ urlpatterns = [
     path('listings/<pk>/delete/', listing_delete),
     path('add-listing/', listing_create)
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
